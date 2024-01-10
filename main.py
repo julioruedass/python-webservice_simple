8aae9520954fc2e4f81187f1db5bae73e28f2ee9
julioruedass <deviidssotoo@gmail.com>
2024-01-10 02:32:22 -0600

    iniciar repocitorio de web service simpole

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify({'status': 'active','message': 'index','code':200})
+
+@app.route('/home')
+def home():
+    return jsonify({'status': 'active','message': "home",'code':200})
+
+@app.route('/error')
+def error():
+    return jsonify({'status': 'inactive','message': "error",'code':0})
+
+if __name__ == '__main__':
+    app.run() 
